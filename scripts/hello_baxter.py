450fb0a77f85a6b8dfc038939ea7c4f8ba5a9b06
markfmccormick <markfmccormick@gmail.com>
2018-02-07 14:57:05 +0000

    Added iniitial position of a8 to hello_baxter.py

--- scripts/hello_baxter.py
+++ scripts/hello_baxter.py
@@ -5,27 +5,45 @@ import baxter_interface
 
 rospy.init_node('Hello_Baxter')
 
-limb = baxter_interface.Limb('right')
-angles = limb.joint_angles()
+right = baxter_interface.Limb('right')
+left = baxter_interface.Limb('left')
 
-# print angles
+base_right = {'right_s0': 0.08, 'right_s1': -1.0, 
+	'right_e0': 1.19, 'right_e1': 1.94, 
+	'right_w0': -0.67, 'right_w1': 1.03, 'right_w2': 0.50}
+base_left = {'left_s0': 1.0, 'left_s1': -1.0, 
+	'left_e0': -1.19, 'left_e1': 1.94, 
+	'left_w0': 0.67, 'left_w1': 1.03, 'left_w2': -0.50}
 
-angles['right_s0']=0.0
-angles['right_s1']=0.0
-angles['right_e0']=0.0
-angles['right_e1']=0.0
-angles['right_w0']=0.0
-angles['right_w1']=0.0
-angles['right_w2']=0.0
+above = right.joint_angles()
+above['right_s0']=0.83
+above['right_s1']=-0.45
+above['right_e0']=0.0
+above['right_e1']=0.9
+above['right_w0']=0.0
+above['right_w1']=1.0
+above['right_w2']=0.0
 
-# print angles
+above = {'right_s0': 0.83, 'right_s1': -0.45, 
+	'right_e0': 0.0, 'right_e1': 0.9, 
+	'right_w0': 0.0, 'right_w1': 1.0, 'right_w2': 0.0}
 
-limb.move_to_joint_positions(angles)
+on = right.joint_angles()
+on['right_s0']=0.83
+on['right_s1']=-0.3
+on['right_e0']=0.0
+on['right_e1']=0.95
+on['right_w0']=0.0
+on['right_w1']=1.0
+on['right_w2']=0.0
 
-wave_1 = {'right_s0': -0.459, 'right_s1': -0.202, 'right_e0': 1.807, 'right_e1': 1.714, 'right_w0': -0.906, 'right_w1': -1.545, 'right_w2': -0.276}
+right.move_to_joint_positions(base_right)
+left.move_to_joint_positions(base_left)
 
-wave_2 = {'right_s0': -0.395, 'right_s1': -0.202, 'right_e0': 1.831, 'right_e1': 1.981, 'right_w0': -1.979, 'right_w1': -1.100, 'right_w2': -0.448}
+right.move_to_joint_positions(above)
+right.move_to_joint_positions(on)
+right.move_to_joint_positions(above)
 
-for _move in range(3):
-	limb.move_to_joint_positions(wave_1)
-	limb.move_to_joint_positions(wave_2)
+
+
+
