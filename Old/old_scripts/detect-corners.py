2d4a5b7239025a39039ee9ad6dc55f2ae780ab57
markfmccormick <markfmccormick@gmail.com>
2018-03-17 14:56:42 +0000

    Added further dissertation related updates

--- Old/old_scripts/detect-corners.py
+++ Old/old_scripts/detect-corners.py
@@ -1,9 +1,9 @@
 import cv2
 import numpy as np
 
-filename = 'kinect_images_new/white_front/middle.jpeg'
+filename = 'cropped_image.jpeg'
 img = cv2.imread(filename)
-img = cv2.resize(img, (0,0), fx = 0.5, fy = 0.5)
+#img = cv2.resize(img, (0,0), fx = 0.5, fy = 0.5)
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
 gray = np.float32(gray)
@@ -14,6 +14,8 @@ dst = cv2.dilate(dst,None)
 
 # Threshold for an optimal value, it may vary depending on the image.
 img[dst>0.01*dst.max()]=[0,0,255]
+
+cv2.imwrite('corner_detection.jpeg', img)
 
 cv2.imshow('dst',img)
 cv2.waitKey()
